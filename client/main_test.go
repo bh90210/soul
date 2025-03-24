d936a4f20130ed9d7d2025c8377d6ad1eb4ce37e
bh90210 <ktc@protonmail.ch>
2025-03-24 22:11:41 +0100

    Better pipeline

--- client/main_test.go
+++ client/main_test.go
@@ -3,9 +3,7 @@ package client
 import (
 	"flag"
 	"os"
-	"os/exec"
 	"testing"
-	"time"
 )
 
 func TestMain(m *testing.M) {
@@ -15,36 +13,5 @@ func TestMain(m *testing.M) {
 		os.Exit(0)
 	}
 
-	go func() {
-		cmd := exec.Command("docker", "run", "--name", "soultest", "-p", "2242:2242", "ghcr.io/bh90210/soul:latest")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		err := cmd.Run()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	time.Sleep(time.Second)
-
 	m.Run()
-
-	cmd := exec.Command("docker", "stop", "soultest")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-
-	cmd = exec.Command("docker", "rm", "soultest")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
-	if err != nil {
-		panic(err)
-	}
 }
